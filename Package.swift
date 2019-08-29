91f82686c3c66c4b5100cfa9cb50d0f4440e1674
Jon Layton <me@jonl.io>
2019-08-29 13:24:11 -0400

    Add platform versions

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "CoreRPC",
+    platforms: [
+       .macOS(.v10_12), .iOS(.v10),
+    ],
     products: [
         .library(name: "CoreRPC", targets: ["CoreRPC"]),
     ],
